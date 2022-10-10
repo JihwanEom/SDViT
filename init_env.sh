bebacc2936844d8dedd78459ae0d79647be67898
Eom, Jihwan <jihwan.eom@intel.com>
2022-10-10 09:21:22 +0900

    init_env.sh

---
+++ init_env.sh
@@ -0,0 +1,5 @@
+pip3 install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio==0.8.1 -f https://download.pytorch.org/whl/torch_stable.html
+pip3 install wilds
+pip3 install timm
+pip3 install seaborn
+pip3 install einops
