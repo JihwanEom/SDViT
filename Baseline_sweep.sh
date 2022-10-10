d0ef9ba37906243bfaee1e4ad6673125a980a521
Eom, Jihwan <jihwan.eom@intel.com>
2022-10-10 09:14:13 +0900

    Update bash

--- Baseline_sweep.sh
+++ Baseline_sweep.sh
@@ -1,8 +1,49 @@
-
-    for command in delete_incomplete launch
+for command in delete_incomplete launch
     do
-    python -m domainbed.scripts.sweep ${command} --data_dir=/home/computervision1/DG_new_idea/domainbed/data/ \
-    --output_dir=./domainbed/PACS_Output/ERM_ViT/backbone_DeitSmall/  --command_launcher multi_gpu --algorithms ERM_ViT  \
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_ViT_Deit-S/baseline  --command_launcher multi_gpu --algorithms ERM_ViT  \
     --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
     --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0}"""
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_SDViT_Deit-S/baseline  --command_launcher multi_gpu --algorithms ERM_SDViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0}"""
+
+
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_ViT_Deit-S/EMA  --command_launcher multi_gpu --algorithms ERM_ViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"EMA\":\"True\"}"""
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_SDViT_Deit-S/EMA  --command_launcher multi_gpu --algorithms ERM_SDViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"EMA\":\"True\"}"""
+
+
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_ViT_Deit-S/CutMix  --command_launcher multi_gpu --algorithms ERM_ViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"CutMix\":\"True\"}"""
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_SDViT_Deit-S/CutMix  --command_launcher multi_gpu --algorithms ERM_SDViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"CutMix\":\"True\"}"""
+
+
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_ViT_Deit-S/EMA_CutMix  --command_launcher multi_gpu --algorithms ERM_ViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"EMA\":\"True\",\"CutMix\":\"True\"}"""
+
+    python -m domainbed.scripts.sweep ${command} --data_dir=./domainbed/data/ \
+    --output_dir=./domainbed/PACS/ERM_SDViT_Deit-S/EMA_CutMix  --command_launcher multi_gpu --algorithms ERM_SDViT  \
+    --single_test_envs  --datasets PACS  --n_hparams 1 --n_trials 3  \
+    --hparams """{\"backbone\":\"DeitSmall\",\"batch_size\":32,\"lr\":5e-05,\"resnet_dropout\":0.0,\"weight_decay\":0.0,\"EMA\":\"True\",\"CutMix\":\"True\"}"""
+
     done
